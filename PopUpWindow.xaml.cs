5e78a341655b80ce7a2bff9bde5823f70966ab4c
evan pavan <evan@evanpavan.com>
2020-07-09 13:15:48 -0400

    created popup window

--- PopUpWindow.xaml.cs
+++ PopUpWindow.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,25 +8,18 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace WPFHello
+namespace DesktopNotifier
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for PopUpWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class PopUpWindow : Window
     {
-        public MainWindow()
+        public PopUpWindow()
         {
             InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            //Print message "Hello world" to new dialog box
-            MessageBox.Show("Hello World");
         }
     }
 }
